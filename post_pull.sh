9ec638dabb5792b6ebcc218eb419c0d69944a84d
Davide Coviello <davidecoviello@gmail.com>
2021-06-30 17:44:12 +0200

    applicato modello al rettangolo con la faccia

--- post_pull.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-DEST_DIR=../../notebook/work
-
-jupytext --to $DEST_DIR//ipynb notebook/py/*.py
--- pre_commit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-DEST_DIR=../../notebook/py
-
-jupytext --to $DEST_DIR//py notebook/work/*.ipynb
